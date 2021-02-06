07df87c0354f4bee7394e6b996d2dc6d3dca6882
jai-dewani <jai.dewani.99@gmail.com>
2021-02-06 15:05:41 +0530

    Add reply feature

---
+++ quotes.py
@@ -0,0 +1,7 @@
+quotes = [
+    "Open Source is like Brushing teeth ~Eddie",
+    "If you helped someone you are mentor to that person ~nhcarrigan",
+    "Help atleast 2 person daily ~Eddie",
+    "Collaboration First Code Second ~Eddie",
+    "Green Square a day keeps eddie away ~Eddie"
+]
