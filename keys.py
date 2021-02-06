c779ee53c571e689035f6a79a6cc00280003d543
Karuna Tata <74637789+kcoder63@users.noreply.github.com>
2021-02-06 12:38:17 +0530

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,7 +1,7 @@
 # twitter application consumer keys and access tokens.
 # Remember, keep this information private.
-api_key = 'e7TpS8De2heXh9aX4fl1xbM7a'
-api_secret_key = 'y2TBwADRFQWbP8rnNeKUPM8151GHgnjXh5An1jhqC3sdE7pDS0'
-access_token = '1338862926086586368-QkHT0KpmLRuXDjig5FdugPFcBcbMs9'
-access_token_secret = 'JLSU8QlqjTgOKqgVNe6NmSeRIXDQx8HDIs1kFy0dgbtAT'
-bearer_token = 'AAAAAAAAAAAAAAAAAAAAANjJKgEAAAAATUyCkrAAH%2BzhnPDcV%2BKnrs9f8IY%3DUnTvxcoY5HwlngGlIWsrJ0TEfFb4Xyb3FvMMfRhWl5ZLJ99usC'
+api_key = 'xxxxx'
+api_secret_key = 'xxxx'
+access_token = 'xxxx'
+access_token_secret = 'xxxxx'
+bearer_token = 'AAAAAAAA'
