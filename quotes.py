40398b91b884a8d645f8858f81b0825c6e7cdb26
Karuna Tata <74637789+kcoder63@users.noreply.github.com>
2021-02-16 18:56:01 +0530

    Add files via upload

---
+++ quotes.py
@@ -0,0 +1,14 @@
+quotes = [
+    "Open Source is like Brushing teeth ~Eddie",
+    "If you helped someone you are mentor to that person ~nhcarrigan",
+    "Help atleast 2 person daily ~Eddie",
+    "get your green square",
+    "hey this is a friendly remainder for you to contribute to open source today. Let's get those green squares together"
+    "Collaboration First Code Second ~Eddie",
+    "Green Square a day keeps eddie away ~Eddie",
+    "Open Source is for everyone...yes YOU! ~Eddie",
+    "Community first,Code second ~ EddieHub",
+    "we all are mentors as well as mentees because we all are learning and sharing ~ Eddie",
+    "Remember there is someone who is where you were 1 month ago, who needs your help ~ Eddie",
+    "the best way to learn a spoken language is to live in the country and learn which applies to open source too ~ Eddie"
+]
