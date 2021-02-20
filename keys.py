48d23db6d55c0e93196c509ca466e526470f7543
Karuna Tata <74637789+kcoder63@users.noreply.github.com>
2021-02-20 19:04:06 +0530

    Add files via upload

---
+++ keys.py
@@ -0,0 +1,4 @@
+api_key = 'Nd5s6fNwzQkx5NYnLITHgwo3w'
+api_secret_key = 'nagegGMI8kaxdN0y2JLwGq55GVWl1zYiycCkcw09WQwiBsGOVF'
+access_token = '1338862926086586368-uMdV2ybU3LHT3RZ7Poo5WxUIRCPxFf'
+access_token_secret = 'LW3IAwxq8JQQmtHbvQmusMiSlfnF0QvHSXzLbRndcDYKB'
